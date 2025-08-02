f37f0aa718a22a24286ecc291c5ed34353d1e962
Tobilinho <tobias.fischer10@web.de>
2025-08-02 22:19:38 +0200

    code update

--- js/main.js
+++ js/main.js
@@ -1,87 +1,96 @@
 // js/main.js
 
 const drinks = {
-  "mojito": {
-    name: "Mojito",
-    img: "img/mojito.jpg",
+  "gin-tonics": {
+    name: "Diverse Gin Tonics",
+    img: "img/gin-tonics.jpg",
+    ingredients: [
+      "5 cl Gin deiner Wahl",
+      "Tonic Water",
+      "Eis",
+      "Garnitur: Limette, Gurke, Beeren etc."
+    ],
+    steps: [
+      "Gin über Eis in ein Glas geben",
+      "Mit Tonic Water auffüllen",
+      "Nach Geschmack garnieren"
+    ]
+  },
+  "caipirinha": {
+    name: "Caipirinha",
+    img: "img/caipirinha.jpg",
+    ingredients: [
+      "1 Limette",
+      "2 TL Rohrzucker",
+      "5 cl Cachaça",
+      "Crushed Ice"
+    ],
+    steps: [
+      "Limette achteln und mit Zucker zerdrücken",
+      "Crushed Ice und Cachaça dazugeben",
+      "Umrühren und servieren"
+    ]
+  },
+  "daiquiri": {
+    name: "Daiquiri",
+    img: "img/daiquiri.jpg",
     ingredients: [
       "5 cl weißer Rum",
-      "1 Limette",
-      "2 TL Zucker",
-      "Minze",
-      "Sodawasser",
-      "Eis"
-    ],
-    steps: [
-      "Limette achteln und mit Zucker zerdrücken",
-      "Minze leicht andrücken und hinzufügen",
-      "Rum und Eis dazugeben",
-      "Mit Sodawasser auffüllen"
-    ]
-  },
-  "virgin-mojito": {
-    name: "Virgin Mojito",
-    img: "img/virgin-mojito.jpg",
-    ingredients: [
-      "1 Limette",
-      "2 TL Zucker",
-      "Minze",
-      "Sodawasser",
-      "Eis"
-    ],
-    steps: [
-      "Limette achteln und mit Zucker zerdrücken",
-      "Minze leicht andrücken und hinzufügen",
-      "Eis dazugeben",
-      "Mit Sodawasser auffüllen"
-    ]
-  },
-  "margarita": {
-    name: "Limetten-Margarita",
-    img: "img/margarita.jpg",
-    ingredients: [
-      "4 cl Tequila",
-      "2 cl Cointreau",
       "2 cl Limettensaft",
-      "Salzrand",
+      "1 cl Zuckersirup",
       "Eis"
     ],
     steps: [
-      "Zutaten shaken",
-      "In Glas mit Salzrand geben",
-      "Mit Limettenscheibe garnieren"
+      "Alle Zutaten mit Eis shaken",
+      "In ein gekühltes Glas abseihen"
     ]
   },
-  "cucumber-cooler": {
-    name: "Cucumber Lime Cooler",
-    img: "img/cucumber-cooler.jpg",
-    ingredients: [
-      "Gurkenscheiben",
-      "2 cl Limettensaft",
-      "1 TL Zucker",
-      "Mineralwasser",
-      "Eis"
-    ],
-    steps: [
-      "Gurke und Zucker zerdrücken",
-      "Limettensaft und Eis dazu",
-      "Mit Wasser auffüllen"
-    ]
-  },
-  "gin-basil": {
-    name: "Gin Basil Smash",
-    img: "img/gin-basil.jpg",
+  "gin-rickey": {
+    name: "Gin Rickey",
+    img: "img/gin-rickey.jpg",
     ingredients: [
       "5 cl Gin",
       "2 cl Limettensaft",
-      "Basilikum",
-      "1 TL Zucker",
+      "Sodawasser",
       "Eis"
     ],
     steps: [
-      "Basilikum und Zucker zerdrücken",
-      "Gin und Limettensaft dazu",
-      "Shaken und auf Eis servieren"
+      "Gin und Limettensaft in ein Glas mit Eis geben",
+      "Mit Sodawasser auffüllen",
+      "Leicht umrühren"
+    ]
+  },
+  "ipanema": {
+    name: "Ipanema",
+    img: "img/ipanema.jpg",
+    ingredients: [
+      "1 Limette",
+      "2 TL Rohrzucker",
+      "100 ml Maracujasaft",
+      "Ginger Ale",
+      "Crushed Ice",
+      "Minze (optional)"
+    ],
+    steps: [
+      "Limette achteln und mit Zucker zerdrücken",
+      "Crushed Ice ins Glas geben",
+      "Mit Maracujasaft und Ginger Ale auffüllen",
+      "Optional mit Minze garnieren"
+    ]
+  },
+  "virgin-caipirinha": {
+    name: "Virgin Caipirinha",
+    img: "img/virgin-caipirinha.jpg",
+    ingredients: [
+      "1 Limette",
+      "2 TL Rohrzucker",
+      "Crushed Ice",
+      "Ginger Ale oder Zitronenlimonade"
+    ],
+    steps: [
+      "Limette achteln und mit Zucker zerdrücken",
+      "Crushed Ice dazugeben",
+      "Mit Ginger Ale oder Limonade auffüllen"
     ]
   }
 };
@@ -98,6 +107,7 @@ if (drink) {
     <ul>${drink.ingredients.map(i => `<li>${i}</li>`).join('')}</ul>
     <h2>Zubereitung</h2>
     <ol>${drink.steps.map(s => `<li>${s}</li>`).join('')}</ol>
+    <p><a href="index.html">⬅️ Zurück zur Übersicht</a></p>
   `;
 } else {
   document.getElementById("cocktail").innerHTML = "<p>Rezept nicht gefunden.</p><a href='index.html'>Zurück</a>";
